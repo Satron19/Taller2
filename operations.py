c077e652fe777eb988910399f6d36ce976786f38
samuel.troncoso <satron19@javerianacali.edu.co>
2023-10-22 23:44:35 -0500

    Added subtraction feature

--- operations.py
+++ operations.py
@@ -2,3 +2,9 @@ def add(num_1, num_2):
     result = num_1 + num_2
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
+
+
+def subtract(num_1, num_2):
+    result = num_1 - num_2
+    print(f'{num_1} - {num_2} is equal to {result}')
+    return result
