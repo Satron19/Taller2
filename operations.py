ba3e6e0aead6bc9dc0e7b48eebc7534f217c821f
samuel.troncoso <satron19@javerianacali.edu.co>
2023-10-22 23:52:42 -0500

    Added feature

--- operations.py
+++ operations.py
@@ -27,3 +27,12 @@ def power(num_1, num_2):
     result = num_1 ** num_2
     print(f'{num_1} raised to the power of {num_2} is equal to {result}')
     return result
+
+def modulus(num_1, num_2):
+    if num_2 != 0:
+        result = num_1 % num_2
+        print(f'The modulus of {num_1} and {num_2} is {result}')
+        return result
+    else:
+        print("Error: Cannot divide by zero (modulus operation).")
+        return None
